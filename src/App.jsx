b7ee9395542d8b88f870367959b501f85cce975a
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-24 21:24:36 +0800

    Modern_UI_Website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div>
+          <h1>Hello world!</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
