6971e8de2b7b622e0dc4834b6b438f795ba6e311
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-25 15:17:42 +0800

    pending

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style'
 
 function Biling() {
   return (
-    <div>Biling</div>
+    <section id="product" 
+    className={layout.sectionReverse}>
+  <div className={layout.sectionImgReverse}>
+       <img src={bill} alt="billing"
+       className='w-[100%] h-[100%] relative z-[5]' />
+
+       <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full 
+       white__gradient'>
+
+       </div>
+  </div>
+    </section>
   )
 }
 
