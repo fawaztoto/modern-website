226aa5a819f1d14ccd6dc3327e4e0e42c7570019
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-28 21:21:30 +0800

    css changed lightly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
   </div>
 
 
-  <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+  <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-9'>
      {footerLinks.map((footerLink) => (
       <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
         <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
