52c2c75d352f829fcce90feefa59d6a8714c1c3a
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-30 22:15:46 +0800

    css

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -7,7 +7,7 @@ import Button from './Button'
 const FeatureCard = ({icon, title, content, index }) => (
 
 <div className={`flex flex-row p-6 rounded-[19px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card` }> 
-<div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+<div className={`w-[64px] h-[63px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
   <img  src={icon} alt="icon" 
   className='w-[50%] h-[50%] object-contain'/>
 </div>
@@ -30,7 +30,7 @@ function Business() {
      <div className={layout.sectionInfo}>
      <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden'
       /> we'll handle the money.</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building 
+      <p className={`${styles.paragraph} max-w-[469px] mt-5`}>With the right credit card, you can improve your financial life by building 
         credit, earing rewards and saving money. But with hundreds of 
         credit cards on the market.</p>
        <Button styles="mt-10" /> 
